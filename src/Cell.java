adb8bb36994f74c74ac9fcd47c5ff9b4ff3967ad
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-05-15 21:19:15 +0200

    A little bit of code

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,31 @@
-public class Cell {
+import javax.swing.*;
+import java.awt.*;
 
+public class Cell extends JPanel {
+
+    private boolean[] status = {false, false}; // alive, dead, next generation. Maybe as integer
+
+    private Color[] color = new Color[8]; // 8 different Colors for now can add some more later
+    private int x_pos, y_pos;
+
+    public Cell(int x_pos, int y_pos)
+    {
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+    }
+
+    public Cell(boolean status, int x_pos, int y_pos)
+    {
+
+        setBackground(Color.white);
+    }
+
+
+    public int getX_pos() {
+        return x_pos;
+    }
+    public int getY_pos() {
+        return y_pos;
+    }
 
 }
--- src/Gameboard.java
+++ src/Gameboard.java
@@ -1,2 +1,18 @@
-public class Gameboard {
+import javax.swing.*;
+import java.awt.*;
+
+public class Gameboard extends JInternalFrame {
+
+    private Cell[] board_cells = new Cell[64]; //Wird allet nochmal geändert will erstmal was reinsetzen
+    private int title_nr;
+
+    public Gameboard()
+    {
+        setTitle("Alive " + title_nr);
+        setLayout(new GridLayout(8, 8));
+        setBackground(Color.BLACK);
+        setVisible(true);
+    }
+
+
 }
