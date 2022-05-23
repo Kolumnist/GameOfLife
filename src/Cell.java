7524a681be922d9c2dd24e7f8af472a83f6e0cb5
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-05-23 16:04:43 +0200

    Some little changes

--- src/Cell.java
+++ src/Cell.java
@@ -5,22 +5,27 @@ public class Cell extends JPanel {
 
     private boolean[] status = {false, false}; // alive, dead, next generation. Maybe as integer
 
-    private Color[] color = new Color[8]; // 8 different Colors for now can add some more later
     private int x_pos, y_pos;
+    private Color color_alive, color_dead;
 
-    public Cell(int x_pos, int y_pos)
+    public Cell(int x_pos, int y_pos, Color color_alive, Color color_dead)
     {
         this.x_pos = x_pos;
         this.y_pos = y_pos;
+        this.color_alive = color_alive;
+        this.color_dead = color_dead;
     }
 
-    public Cell(boolean status, int x_pos, int y_pos)
+    public Cell(boolean status, int x_pos, int y_pos, Color color_alive, Color color_dead)
     {
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+        this.color_alive = color_alive;
+        this.color_dead = color_dead;
 
-        setBackground(Color.white);
     }
 
-
+    public void changeStatus(){};
     public int getX_pos() {
         return x_pos;
     }
--- src/Gameboard.java
+++ src/Gameboard.java
@@ -3,14 +3,33 @@ import java.awt.*;
 
 public class Gameboard extends JInternalFrame {
 
+    private Color[] colors_alive = new Color[8]; // 8 different Colors for now can add some more later
+    private Color[] colors_dead = new Color[8]; //for the different colors plus every window has more color
+
     private Cell[] board_cells = new Cell[64]; //Wird allet nochmal geändert will erstmal was reinsetzen
-    private int title_nr;
+
+    private static int title_nr;
 
     public Gameboard()
     {
         setTitle("Alive " + title_nr);
         setLayout(new GridLayout(8, 8));
         setBackground(Color.BLACK);
+        //setJMenuBar();
+
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                board_cells[i*j] = new Cell(j, i, colors_alive[0], colors_dead[0]);
+            }
+        }
+        for(JPanel panels : board_cells)
+        {
+            add(panels);
+        }
+
+        title_nr++;
         setVisible(true);
     }
 
