a2925f843179b5469a5e853cf76cc88d06102244
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-06-02 21:03:45 +0200

    More Figuren schmock

--- src/HardCodingofTheFigures.java
+++ src/HardCodingofTheFigures.java
@@ -38,7 +38,7 @@ public class HardCodingofTheFigures {
         try{ board_cells[c.getX_pos()+2][c.getY_pos()+4].switchAlive();}catch(Exception ignored){}
     }
 
-    public void pentadecathlon(Cell c)
+    public void pentadecathlonVertical(Cell c)
     {
         try{ board_cells[c.getX_pos()+5][c.getY_pos()].switchAlive();}catch(Exception ignored){}
         try{ board_cells[c.getX_pos()+5][c.getY_pos()+1].switchAlive();}catch(Exception ignored){}
@@ -64,6 +64,34 @@ public class HardCodingofTheFigures {
         try{ board_cells[c.getX_pos()-3][c.getY_pos()].switchAlive();}catch(Exception ignored){}
         try{ board_cells[c.getX_pos()-3][c.getY_pos()-1].switchAlive();}catch(Exception ignored){}
         try{ board_cells[c.getX_pos()-3][c.getY_pos()+1].switchAlive();}catch(Exception ignored){}
+    }
+
+    public void pentadecathlonHorizontal(Cell c)
+    {
+        try{ board_cells[c.getX_pos()][c.getY_pos()+5].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()+5].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()+5].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()+4].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()+3].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()+2].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()+2].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()+2].switchAlive();}catch(Exception ignored){}
+
+        try{ board_cells[c.getX_pos()][c.getY_pos()].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()-1].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()-1].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()-1].switchAlive();}catch(Exception ignored){}
+
+        try{ board_cells[c.getX_pos()][c.getY_pos()-6].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()-6].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()-6].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()-5].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()-4].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()][c.getY_pos()-3].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()-1][c.getY_pos()-3].switchAlive();}catch(Exception ignored){}
+        try{ board_cells[c.getX_pos()+1][c.getY_pos()-3].switchAlive();}catch(Exception ignored){}
     }
 
     public void glider(Cell c)
